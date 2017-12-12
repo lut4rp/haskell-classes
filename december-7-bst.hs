b87b7c30dd4beed07922830e2381ea7510aee4b5
Pratul Kalia <pratul@uncommon.is>
2017-12-12 15:08:07 +0530

    bfind and binsert - binsert unworking

--- december-7-bst.hs
+++ december-7-bst.hs
@@ -11,13 +11,52 @@ module BST where
 --             deriving (Show)
 
 data BST = Empty
-         | Node BST Int BST
+         | Node { left :: BST, val :: Int, right :: BST }
          deriving (Show)
 
-binsert :: BST -> Int -> BST
+bfind    :: BST -> Int -> Bool
+binsert  :: BST -> Int -> BST
+
+bfind Empty _num = False
+
+bfind mybst num =
+  num == val mybst ||
+  if num > val mybst
+    then bfind (right mybst) num
+    else bfind (left  mybst) num
 
 binsert Empty num = Node Empty num Empty
-binsert (Node left val right) num =
-  if num > val
-    then undefined
-    else undefined
+
+binsert mybst num =
+  if num > val mybst then
+    if right mybst == Empty then
+      Node (left mybst) (val mybst) (Node Empty num Empty) else
+      binsert (right mybst) num
+  else if left mybst == Empty then
+    Node (Node Empty num Empty) (val mybst) (right mybst) else
+    binsert (left mybst) num
+
+
+-- Node {
+--   left = Node {
+--     left = Node {
+--       left = Empty, val = 1, right = Empty
+--     },
+--     val = 5,
+--     right = Node {
+--       left = Empty,
+--       val = 6,
+--       right = Empty
+--     }
+--   },
+--   val = 10,
+--   right = Node {
+--     left = Node {
+--       left = Empty,
+--       val = 17,
+--       right = Empty
+--     },
+--     val = 19,
+--     right = Empty
+--   }
+-- }
