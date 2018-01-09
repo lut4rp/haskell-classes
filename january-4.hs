519b3e2d648098b238e0d0ee774105bbaf9796d5
Pratul Kalia <pratul@uncommon.is>
2018-01-09 17:20:58 +0530

    updated json data type with Show impl

--- january-4.hs
+++ january-4.hs
@@ -3,25 +3,31 @@ module January4 where
 -- Write a data type to represent JSON
 -- Implement "Show" on it to print it. Or pretty print maybe?
 
-data JObject = JObject String JValue JObject
-             | JObjectEmpty
-             | JObjectEnd
-             deriving (Show)
+newtype JObject = JObject [JRecord]
+instance Show JObject where
+  show (JObject jrecords) = "{" ++ show jrecords ++ "}"
 
-data JArray = JArray JValue JArray
-            | JArrayEmpty
-            | JArrayEnd
-            deriving (Show)
 
-data JValue = JValueObject JObject
-            | JValueArray JArray
+newtype JArray = JArray [JValue]
+instance Show JArray where
+  show (JArray jvalues) = show jvalues
+
+
+data JRecord = JRecord String JValue
+instance Show JRecord where
+  show (JRecord key jvalue) = show key ++ ":" ++ show jvalue
+
+
+data JValue = JValueArray JArray
+            | JValueObject JObject
             | JValueString String
             | JValueInt Int
             | JValueBool Bool
             | JValueNull
-            deriving (Show)
-
-data JSON = WithObject JObject
-          | WithArray JArray
-          | WithValue JValue
-          deriving (Show)
+instance Show JValue where
+  show (JValueObject o) = show o
+  show (JValueArray a) = show a
+  show (JValueString s) = show s
+  show (JValueInt i) = show i
+  show (JValueBool b) = show b
+  show JValueNull = "null"
